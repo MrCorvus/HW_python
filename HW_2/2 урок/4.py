8a1e14b507ded54a64db940ca4712a24c3ba65d6
MrCorvus <33394578+MrCorvus@users.noreply.github.com>
2021-12-04 17:56:43 +0300

    Update 4.py

--- HW_2/2 урок/4.py
+++ HW_2/2 урок/4.py
@@ -3,9 +3,7 @@ counts = [str(e) for e in counts]
 
 
 for i, e in enumerate(counts):
-    if len(e) == 4:
-        counts[i] = f'{e[0:2]}.0{e[3:]}'
-    if len(e) == 2:
+    if len(e) in [2 , 4]:
         counts[i] = f'{float(e):.02f}'
 for i, e in enumerate(counts):
     if len(e) == 5:
@@ -17,3 +15,4 @@ print(counts)
 rev_counts = list(reversed(counts))
 print(rev_counts)
 print(list(sorted(rev_counts[0:5])))
+
